385cbe434bb22b0fc96dbe03cbbe88b3059821de
AdeuxX <antoine.lebescond@gmail.com>
2025-05-10 14:53:46 +0200

    etape2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# main.py - Squelette Pygame de base
+# main.py - Simulation de boule sur trajectoire circulaire
 
 # ---------------------------
 # Import des modules
@@ -13,12 +13,26 @@ pygame.init()
 
 # Création de la fenêtre principale
 screen = pygame.display.set_mode((1920, 1080))
-pygame.display.set_caption("Projet Pygame")
+pygame.display.set_caption("Simulation de mouvement circulaire")
 background_color = (0, 0, 0)  # Noir
 
 # Configuration de l'horloge pour contrôler les FPS
 clock = pygame.time.Clock()
 FPS = 45
+
+# ---------------------------
+# Configuration des objets
+# ---------------------------
+# Cercle fixe (trajectoire)
+CIRCLE_POS = (960, 540)       # Centre de l'écran
+CIRCLE_RADIUS = 400
+CIRCLE_COLOR = (255, 255, 255)
+
+# Boule mobile
+BALL_RADIUS = 20
+BALL_COLOR = (255, 255, 255)
+ball_pos = [CIRCLE_POS[0], CIRCLE_POS[1] - CIRCLE_RADIUS]  # Position initiale au sommet
+ball_velocity = [0, 0]  # Vitesse initiale (vx, vy)
 
 # ---------------------------
 # Boucle principale du jeu
@@ -34,9 +48,15 @@ while running:
             running = False
     
     # ---------------------------
-    # Rafraîchissement de l'écran
+    # Dessin des éléments
     # ---------------------------
     screen.fill(background_color)
+    
+    # Dessin du cercle de trajectoire
+    pygame.draw.circle(screen, CIRCLE_COLOR, CIRCLE_POS, CIRCLE_RADIUS, 1)  # Dernier paramètre = épaisseur
+    
+    # Dessin de la boule mobile
+    pygame.draw.circle(screen, BALL_COLOR, (int(ball_pos[0]), int(ball_pos[1])), BALL_RADIUS)
     
     # ---------------------------
     # Mise à jour de l'affichage
